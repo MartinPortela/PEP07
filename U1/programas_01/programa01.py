981b3f4075acafa1ca1bad60915c5c5f873f3b52
Martín Portela <martinportelasegui@gmail.com>
2025-09-24 12:45:35 +0200

    Comienzo ejercicios

---
+++ U1/programas_01/programa01.py
@@ -0,0 +1,3 @@
+#Escribe un programa que muestre por pantalla tú nombre y apellidos.
+
+print("Mi nombre es Martín Portela Seguí")
---
+++ U1/programas_01/programa02.py
@@ -0,0 +1,3 @@
+#Escribe un programa que escriba por pantalla: el booleano True, dos literales enteros,
+#dos literales flotantes y una cadena de caracteres, y para cada uno de ellos su tipo de
+#datos.
