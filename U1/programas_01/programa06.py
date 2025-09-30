7b7366a30ea2f9f11f99a440fa55a671b6907297
Martín Portela <martinportelasegui@gmail.com>
2025-09-30 10:49:32 +0200

    Terminado el primer tema

--- U1/programas_01/programa06.py
+++ U1/programas_01/programa06.py
@@ -9,3 +9,27 @@
 #Mostrar las operaciones de los operadores de comparación de Python con valores
 #booleanos y/o números.
 
+a=5
+b=6
+
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
+print(f"{a} // {b} = {a//b}")
+print(f"{a} & {b} = {a&b}")
+print(f"{a} ** {b} = {a**b}\n\n")
+
+x=True
+y=False
+
+print(f"{x} and {y} {x and y}")
+print(f"{x} or {y} {x or y}")
+print(f"not {x} {not x}\n\n")
+
+print(f"{a} == {b} {a==b}")
+print(f"{a} != {b} {a!=b}")
+print(f"{a} > {b} {a>b}")
+print(f"{a} < {b} {a<b}")
+print(f"{a} >= {b} {a>=b}")
+print(f"{a} <= {b} {a<=b}")
---
+++ U1/programas_01/programa07.py
@@ -0,0 +1,5 @@
+#Escribe un programa que use la función print() para
+#Escribir un el número 34 en binario, otro en octal y otro en hexadecimal.
+print(f"binario: {bin(34)}, octal: {oct(34)} y hexadecimal: {hex(34)}")
+#Escribir los números 0b1001, 0o12, 0xa3f en decimal.
+print(f"Binario a decimal: {int("0b1001", 2)}, octal a decimal: {int("0o12", 8)} y de hexadecimal a decimal: {int("0xa3f", 16)}")
