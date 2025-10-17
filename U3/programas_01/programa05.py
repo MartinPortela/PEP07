9e3b75926187b575faa29e486bbac89d43ae5160
Martín Portela <martinportelasegui@gmail.com>
2025-10-17 09:40:30 +0200

    Terminados los ejercicios

--- U3/programas_01/programa05.py
+++ U3/programas_01/programa05.py
@@ -1,2 +1,18 @@
 #Escribe un programa donde crees varias funciones y pruebes el ámbito de las variables
 #en Python (globales, no locales y locales).
+
+def mostrar():
+    b=20 #local
+    global a
+    print(a)
+    a=23
+    print(a+b)
+    print(a)
+    c=22 #nonlocal, no cambiará la global
+    print(c)
+    return
+
+a=15 #variable global
+c=30 #variable global
+mostrar()
+print(c)
