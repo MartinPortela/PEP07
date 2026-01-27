a8026e62cd72440756e79b4995e18f9f60af2e74
Martín Portela <martinportelasegui@gmail.com>
2026-01-27 12:36:25 +0100

    Blog django

---
+++ U6/05_django_blog/blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin): # uevo​
+    list_display = (
+    "titulo",
+    "autor",
+    "cuerpo",
+    )
+
+admin.site.register(Post, PostAdmin)
---
+++ U6/05_django_blog/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
+    cuerpo = models.TextField()
+
+    def __str__(self):
+        return self.titulo
